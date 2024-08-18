a4a4172ce4595de67c3d8f6b40e6ef83bfc441cb
Yuriy Plyska <Yuriyply@gmail.com>
2024-08-18 12:37:57 +0300

    added another picture

---
+++ dist/changePicture.js
@@ -0,0 +1,9 @@
+
+window.onload = function() {
+   var image = document.getElementById('responsiveImage');
+   
+   if (window.innerWidth <= 768) { 
+       image.src = '/html/img/slider/slide-3.jpg';
+   }
+};
+
